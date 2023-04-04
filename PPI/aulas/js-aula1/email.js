e1650d8768abba2e3c23177fc6efd828caf8ec05
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-04-04 06:44:06 -0300

    Add files via upload

---
+++ PPI/aulas/js-aula1/email.js
@@ -0,0 +1,5 @@
+/*let email = 'batista.o@escolar.ifrn.edu.br';*/
+let email = prompt('Digite seu email');
+let nome = email.split('@')[0];
+/*console.log(`Nome do usuário: ${nome}`);*/
+alert(`Nome do usuário: ${nome}`);
