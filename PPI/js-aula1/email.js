146afa3ea9d2af85b455a33cd8fd6cfc5acbc64f
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-04-04 06:44:40 -0300

    Delete PPI/js-aula1 directory

--- PPI/js-aula1/email.js
+++
@@ -1,5 +0,0 @@
-/*let email = 'batista.o@escolar.ifrn.edu.br';*/
-let email = prompt('Digite seu email');
-let nome = email.split('@')[0];
-/*console.log(`Nome do usuário: ${nome}`);*/
-alert(`Nome do usuário: ${nome}`);
--- PPI/js-aula1/pot.js
+++
@@ -1,4 +0,0 @@
-let x = 2; 
-let y = 3;
-console.log('Resultado de ' + x + ' elevado a ' + y + ' é igual a ' + Math.pow(x,y));
-console.log(`Resultado de ${x} elevado a ${y} é igual a ${x ** y}`)
