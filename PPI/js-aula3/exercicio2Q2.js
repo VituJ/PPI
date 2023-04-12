1e6bd93d6e20d3c8bed46ba5ccaa79597ca2d7d3
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-04-12 14:27:41 -0300

    Add files via upload

---
+++ PPI/js-aula3/exercicio2Q2.js
@@ -0,0 +1,10 @@
+let estudantes = ['Adib', 'Fernanda', 'John', 'Poliana', 'Samuel', 'Milena', 'Edilson'];
+
+console.log(estudantes[4]);
+console.log(estudantes.length);
+estudantes.push('Raissa', 'Laura', 'Plácida');
+console.log(estudantes);
+delete estudantes[2];
+delete estudantes[3];
+delete estudantes[4];
+console.log(estudantes);
