dc30b38d7e49aea3282b4521f0140f43863904d1
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-04-26 13:36:21 -0300

    Add files via upload

--- PPI/exercicio-js-lista3repeticao/q1.js
+++ PPI/exercicio-js-lista3repeticao/q1.js
@@ -2,10 +2,10 @@ let login = prompt('Login: ');
 let senha = prompt('Senha: ');
 while (true){
     if  ( login == senha){
-        document.write(`Login e senha precisam ser diferentes.`)
+        document.write(`<h2>Login e senha precisam ser diferentes.</h2>`)
         break;
     } else {
-        document.write(`OK!`);
+        document.write(`<h2>OK!</h2>`);
         break;
     }
 }
