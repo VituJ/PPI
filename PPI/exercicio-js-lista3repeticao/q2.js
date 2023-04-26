d671318b1ae5e1474d5faca3192a331682247c4a
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-04-26 14:17:07 -0300

    Add files via upload

--- PPI/exercicio-js-lista3repeticao/q2.js
+++ PPI/exercicio-js-lista3repeticao/q2.js
@@ -1,9 +1,9 @@
 let array = [25, 49, 7, 4, 17, 44, 11, 23, 5, 1];
 let x = 1;
 document.write(`<h2>A lista desordenada corresponde a: ${array}</h2>`);
-while (x < array.length){
-    y = array[x];
-    z = x-1;
+while (x < array.length){  
+    y = array[x];   
+    z = x-1; 
     while(z >= 0 && array[z] > y){
         array[z+1] = array[z];
         z-=1; 
