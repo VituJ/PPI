888ed1124739d92f00f7d4f94e5ca9947454316d
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-05-03 14:25:33 -0300

    Add files via upload

--- PPI/exercicios-js-lista4repeticao2/q1.js
+++ PPI/exercicios-js-lista4repeticao2/q1.js
@@ -1,15 +1,17 @@
-qtd_questoes = +prompt('Digite a quantidade de questões da prova: ');
-gabarito = prompt ('Digite o gabarito da questão: ');
-respostas = prompt ('Digite a sua resposta da questão: ');
-acertos = 0;
-i = 0;
+let qtd_questoes = +prompt('Digite a quantidade de questões da prova: ');
+let gabarito = prompt ('Digite o gabarito da questão: ');
+let respostas = prompt ('Digite a sua resposta da questão: ');
+let acertos = 0;
+let i = 0;
 if (qtd_questoes == respostas.length && qtd_questoes == gabarito.length){
     while(i < qtd_questoes){
         if (gabarito[i] == respostas[i]){
             acertos++;
         }
         i++;
-    }  
+    }
+    document.write(`<h2>A quantidade de acertos foi de ${acertos}</h2>`)
 } else{
     document.write(`<h2>Digite uma quantidade de respostas ou um gabarito que condiza com a quantidade de questões.</h2>`)
 }
+
