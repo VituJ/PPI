2e12798e802bf63cc63b7a35cb24b4433643a95b
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-07-05 13:33:08 -0300

    Delete PPI-TS directory

--- PPI-TS/principal.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cachorro_1 = require("./cachorro");
-//let bob: Cachorro //Opção 1 de criar um objeto
-//Opção 2 de criar:
-var Rex = new cachorro_1.Cachorro('Tex Rex', 'Caramelo');
-var perola = new cachorro_1.Cachorro('Perola', 'Vira_TODAS_As_Latas');
-console.log(Rex);
-console.log(perola);
