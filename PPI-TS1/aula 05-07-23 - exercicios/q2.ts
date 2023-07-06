a1a5ad48b9664af81da620a2e3f9d89eee459863
João Victor Batista <105079854+VituJ@users.noreply.github.com>
2023-07-06 17:08:53 -0300

    Update q2.ts

--- PPI-TS1/aula 05-07-23 - exercicios/q2.ts
+++ PPI-TS1/aula 05-07-23 - exercicios/q2.ts
@@ -5,10 +5,41 @@ export class Contato {
     private telefone: number = 0;
 
     constructor (nm: string, mail: string, fone: number) {
-        this.nome = nm;
+        this.alterarNome(nm);
+        this.alterarEmail(mail);
+        this.alterarFone(fone);
+
+        /*this.nome = nm;
         this.email = mail;
-        this.telefone = fone;
+        this.telefone = fone;*/
 
     }
 
+    alterarNome(n: string): string {
+        if (n != '') {
+            this.nome = n;
+            return 'true';
+        } 
+        return 'false';
+    }
+
+    alterarEmail(n: string): string {
+        if (n != '') {
+            this.email = n;
+            return 'true';
+        } 
+        return 'false';
+    }
+
+    alterarFone(n: number): number {
+        if (n != 0) {
+            this.telefone = n;
+            return 2;
+        } 
+        return 3;
+    }
+
 }
+
+let email1 = new Contato('José', 'Araujo@gmail.com', 0);
+console.log(email1);
